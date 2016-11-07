1540e84d067f0f633262ede14c5426ee44df419d
andres-aguayo <andres.aguayo@ymail.com>
2016-11-07 13:05:56 -0500

    reverse (working code, to be cleaned)

---
+++ APIreverse.java
@@ -0,0 +1,121 @@
+/****************
+ * Andres Aguayo
+ * CODE2040
+ * Step 2
+ ****************/
+
+import java.net.*;
+import java.io.*;
+import org.json.simple.JSONObject;
+
+public class APIreverse {
+
+	public final static void main(String[] args) {
+		
+		String target = "http://challenge.code2040.org/api/reverse";
+		HttpURLConnection connection = null;
+		
+		try {
+		
+			URL url = new URL(target);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("POST");
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			connection.setUseCaches(false);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.connect();
+			
+			JSONObject obj = new JSONObject();
+		
+			obj.put("token", "e8e73cbfeb50a85f4d013b3851cd7917");
+			
+			DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+			out.writeBytes(obj.toString());
+			out.flush();
+			out.close();
+			
+			InputStream in = connection.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(in));
+			
+			String line;
+			line = rd.readLine();
+			System.out.println(line);
+			
+			rd.close();
+			
+			char[] chars = line.toCharArray();
+			int last = chars.length-1;
+			int mid = chars.length/2;
+			char tmp;
+			
+			for(int i=0; i<mid; i++) {
+				
+				tmp = chars[i];
+				chars[i] = chars[last-i];
+				chars[last-i] = tmp;
+				
+			}
+			
+			String reverse = new String(chars);
+			System.out.println(reverse);
+			
+			target = "http://challenge.code2040.org/api/reverse/validate";
+			
+			try {
+				
+				url = new URL(target);
+				connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("POST");
+				connection.setDoInput(true);
+				connection.setDoOutput(true);
+				connection.setUseCaches(false);
+				connection.setRequestProperty("Content-Type", "application/json");
+				connection.connect();
+				
+				obj = new JSONObject();
+			
+				obj.put("token", "e8e73cbfeb50a85f4d013b3851cd7917");
+				obj.put("string", reverse);
+				
+				out = new DataOutputStream(connection.getOutputStream());
+				out.writeBytes(obj.toString());
+				out.flush();
+				out.close();
+				
+				in = connection.getInputStream();
+				rd = new BufferedReader(new InputStreamReader(in));
+				StringBuffer response = new StringBuffer();
+				
+				while((line = rd.readLine()) != null) {
+					response.append(line);
+					response.append('\r');
+				}
+				
+				rd.close();
+				System.out.print(response.toString());
+				
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+				
+			} finally {
+				
+				connection.disconnect();
+				
+			}
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		} finally {
+			
+			connection.disconnect();
+			
+		}
+		
+		
+	}
+	
+}
